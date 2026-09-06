af6e536ae597b44804d9c3e2eb9784334788d8d1
chandrasekhar6626 <chandrasekhar8977977832@gmail.com>
2026-09-06 22:01:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ const defaultBranches = [
   { name: "NANDYAL", code: "TTD" },
   { name: "NANDYAL", code: "NGO" },
   { name: "MYDUKUR", code: "MYD" }
+  { name: "KADAPA", code: "KDP" }
 ];
 
 const insertDefaultBranch = db.prepare(`
